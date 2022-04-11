aa76bd0cb367df409a9f983592505fd16e0700e2
Carol Ernst <91160233+carolernst-uzh@users.noreply.github.com>
2022-04-10 15:07:53 -1000

    Created Nav Bar and Start Page with button to create new Session. closes #24

---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "styles/ui/Navbar.scss";
+import {Link, useHistory} from "react-router-dom";
+import logoutIcon from "../../img/logout.png";
+import logoIcon from "../../img/logo.png"
+
+export const Navbar = props => {
+    const history = useHistory();
+    const doLogout = () => {
+        localStorage.removeItem('token');
+        history.push('/login');
+    }
+    const goHome = () => {
+        history.push('/game');
+    }
+    return (
+      <div className="nav-bar-container-light">
+          <div className="left-item">
+              <li className="list-item">
+              <button
+                className="nav-button"
+                onClick={() => goHome()}>
+                  <img className="home-icon" src={logoIcon} alt="home"/>
+              </button>
+              </li>
+          </div>
+          <ul className="middle-items">
+          </ul>
+          <ul className="right-items">
+              <li className="list-item">
+                  <button
+                      className= "nav-button"
+                      onClick={() => doLogout()} >
+                  <img className="icon" src={logoutIcon} alt="logout"/>
+                  </button>
+              </li>
+          </ul>
+      </div>
+  )}
---
+++ src/components/views/NewSession.js
@@ -0,0 +1,9 @@
+
+
+const NewSession = () => {
+    return (
+        <div> Under Construction </div>
+    )
+}
+
+export default NewSession;
