6fcfd94b2c9c770e5b20587c324e963b6d073be8
Carol Ernst <91160233+carolernst-uzh@users.noreply.github.com>
2022-05-16 14:22:33 +0200

    Trying to merge

--- src/components/views/Profile.js
+++ src/components/views/Profile.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import {useHistory, useParams} from 'react-router-dom';
 import "styles/views/Profile.scss";
 import {Navbar} from "../ui/Navbar";
@@ -6,7 +8,6 @@ import UploadAvatar from 'components/firebase comps/uploadAvatar';
 import PropTypes from "prop-types";
 import noAvatar from "../../img/noAvatar.png";
 import {Button6} from "../ui/Button";
-import {useEffect, useState} from "react";
 
 const FormField = props => {
     return (
@@ -30,112 +31,112 @@ FormField.propTypes = {
 
 
 const Profile = () => {
-  // use react-router-dom's hook to access the history
-  const history = useHistory();
-  const userId = useParams().userId;
-  const [user, setUser] = useState([]);
-  const [file, setFile] = useState(null);
-  const [error, setError] = useState(null);
-  const [avatarUrl, setAvatarUrl] = useState(null);
+    // use react-router-dom's hook to access the history
+    const history = useHistory();
+    const userId = useParams().userId;
+    const [user, setUser] = useState([]);
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+    const [avatarUrl, setAvatarUrl] = useState(null);
 
-  const types = ['image/png', 'image/jpeg'];
-
-
-  const handleAvatarChange = (e) => {
-    let selected = e.target.files[0]; // to select the first file (in order someone selects more files)
-    console.log(selected); 
-    
-    if (selected && types.includes(selected.type)) {
-      setFile(selected);
-      setError('');
-    } else {
-      setFile(null);
-      setError('Please select an image file (png or jpg)');
-    }
-  };
-
-  const editProfile = (userId) => {
-    try {
-      if(localStorage.getItem('userId') == user.userId){history.push(`/game/profile/${user.userId}/edit`)}
-      
-      else{alert("You can't access this profile page");}
-     } catch (error) {
-        console.error(`Something went wrong while trying to edit the user: \n${handleError(error)}`);
-        console.error("Details:", error);
-        alert("Something went wrong when trying to edit the profile! See the console for details.");
-    }
-  };
+    const types = ['image/png', 'image/jpeg'];
 
 
-  // the effect hook can be used to react to change in your component.
-  // in this case, the effect hook is only run once, the first time the component is mounted
-  // this can be achieved by leaving the second argument an empty array.
-  // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
-  useEffect(() => {
-    // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
-    const fetchData = async () => {
-      try {
-        const response =  await api.get('/users/'+ userId );
+    const handleAvatarChange = (e) => {
+        let selected = e.target.files[0]; // to select the first file (in order someone selects more files)
+        console.log(selected);
 
-        setUser(response.data);
+        if (selected && types.includes(selected.type)) {
+            setFile(selected);
+            setError('');
+        } else {
+            setFile(null);
+            setError('Please select an image file (png or jpg)');
+        }
+    };
 
-        console.log('request to:', response.request.responseURL);
-        console.log('status code:', response.status);
-        console.log('status text:', response.statusText);
-        console.log('requested data:', response.data);
-      } catch (error) {
-        console.error(`Something went wrong while getting the data for the user: \n${handleError(error)}`);
-        console.error("Details:", error);
-        alert("Something went wrong while getting the data for the user! See the console for details.");
-      }
+    const editProfile = (userId) => {
+        try {
+            if(localStorage.getItem('userId') == user.userId){history.push(`/game/profile/${user.userId}/edit`)}
 
-    }
+            else{alert("You can't access this profil page");}
+        } catch (error) {
+            console.error(`Something went wrong while trying to edit the user: \n${handleError(error)}`);
+            console.error("Details:", error);
+            alert("Something went wrong when trying to edit the profile! See the console for details.");
+        }
+    };
 
-      fetchData()
+
+    // the effect hook can be used to react to change in your component.
+    // in this case, the effect hook is only run once, the first time the component is mounted
+    // this can be achieved by leaving the second argument an empty array.
+    // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
+    useEffect(() => {
+        // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
+        const fetchData = async () => {
+            try {
+                const response =  await api.get('/users/'+ userId );
+
+                setUser(response.data);
+
+                console.log('request to:', response.request.responseURL);
+                console.log('status code:', response.status);
+                console.log('status text:', response.statusText);
+                console.log('requested data:', response.data);
+            } catch (error) {
+                console.error(`Something went wrong while getting the data for the user: \n${handleError(error)}`);
+                console.error("Details:", error);
+                alert("Something went wrong while getting the data for the user! See the console for details.");
+            }
+
+        }
+
+        fetchData()
     }, []);
 
-  let uploadAvatar = (
-    <div className = "profile form">
-    <div className = "uploadAvatar">
-      <div className = "uploadAvatar input">
-      <input type="file" src= {user !== [] && user.avatarUrl} onChange={handleAvatarChange}/>
-      </div>
-      { error && <div className="uploadAvatar output"><div className="error">{ error }</div></div>}
-      { (file || avatarUrl) && <UploadAvatar file={file} setFile={setFile} avatarUrl={avatarUrl} setAvatarUrl={setAvatarUrl} />}
-    </div>
-    </div>
-  )
-
-  let editProfileButton = (
-    <div>
-        <Button6 width = "100%" 
-        onClick={() => editProfile(user.userId)}>
-            Edit Profile
-        </Button6>
-    </div>
-)
-
-
-  return (
-      <div><Navbar />
-      <div className="profile" >
-      <div className="profile container">
-            <div className="profile form">
-            <div className = "avatar">
-                { user.avatarUrl && user && (<img alt="Avatar" src={user.avatarUrl}></img>)}
-                { !user.avatarUrl && (<img alt="Avatar" src={noAvatar}></img>)}
+    let uploadAvatar = (
+        <div className = "profile form">
+            <div className = "uploadAvatar">
+                <div className = "uploadAvatar input">
+                    <input type="file" src= {user !== [] && user.avatarUrl} onChange={handleAvatarChange}/>
                 </div>
-
-                {user.username && user && <h1>{user.username}</h1>}
-                {user.name && user &&<h2>Name: {user.name}</h2>}
-                {user.bio && user && <h2>Bio: {user.bio}</h2>}
-                {localStorage.getItem('userId') == user.userId && editProfileButton}
+                { error && <div className="uploadAvatar output"><div className="error">{ error }</div></div>}
+                { (file || avatarUrl) && <UploadAvatar file={file} setFile={setFile} avatarUrl={avatarUrl} setAvatarUrl={setAvatarUrl} />}
             </div>
-      </div>
-      </div>
-      </div>
+        </div>
+    )
 
-  );
+    let editProfileButton = (
+        <div>
+            <Button6 width = "100%"
+                     onClick={() => editProfile(user.userId)}>
+                Edit Profile
+            </Button6>
+        </div>
+    )
+
+
+    return (
+        <div><Navbar />
+            <div className="profile" >
+                <div className="profile container">
+                    <div className="profile form">
+                        <div className = "avatar">
+                            { user.avatarUrl && user && (<img alt="Avatar" src={user.avatarUrl}></img>)}
+                            { !user.avatarUrl && (<img alt="Avatar" src={noAvatar}></img>)}
+                        </div>
+
+                        {user.username && user && <h1>{user.username}</h1>}
+                        {user.name && user &&<h2>Name: {user.name}</h2>}
+                        {user.bio && user && <h2>Bio: {user.bio}</h2>}
+                        {localStorage.getItem('userId') == user.userId && editProfileButton}
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
 }
 
 export default Profile;
