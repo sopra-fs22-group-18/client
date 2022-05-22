d1c7b6b7f71b32d1b90272f9a23a11362d54a56b
trofej <timon.fopp@uzh.ch>
2022-05-23 01:58:24 +0200

    I edited the profile page and edit page, which was not working properly - closes #5

--- src/components/firebase comps/uploadAvatar.js
+++ src/components/firebase comps/uploadAvatar.js
@@ -2,32 +2,19 @@ import React, { useEffect } from 'react';
 import useStorage from '../firebase hooks/useStorage';
 import {api, handleError} from "../../helpers/api";
 
-const UploadAvatar = ({ file, setFile, setAvatarUrl, userId}) => {
+const UploadAvatar = ({ file, setFile, setAvatarUrl}) => {
   const {url} = useStorage(file);
   console.log(url);
 
   useEffect(() => {
     if (url) {
       setAvatarUrl(url);
-      
-      const updateAvatar = async () => {
-        try {
-            const requestBody = JSON.stringify({url});
-            const response = await api.put('/users/' + userId, requestBody);
-    
-
-        } catch (error) {
-            alert(`Something went wrong when trying to update the avatarUrl: \n${handleError(error)}`);
-        }
-        updateAvatar();
-      }
     }
   }, [url, setFile, setAvatarUrl]);
 
 
   return (
-      <img src="" alt="" id="image">
-      </img>
+   <text>Avatar has been uploaded!</text>
   );
 } 
 
--- src/components/views/EditProfile.js
+++ src/components/views/EditProfile.js
@@ -1,11 +1,14 @@
-import React, {useState} from 'react';
+import { useState, useEffect } from 'react';
+import {useHistory, useParams} from 'react-router-dom'
 import {api, handleError} from 'helpers/api';
-import {useHistory} from 'react-router-dom';
 import {Button} from 'components/ui/Button';
 import 'styles/views/EditProfile.scss';
 import BaseContainer from "components/ui/BaseContainer";
 import PropTypes from "prop-types";
 import ProgressBar from 'components/firebase comps/uploadImage'
+import {Navbar} from "../ui/Navbar";
+import noAvatar from "../../img/noAvatar.png";
+import UploadAvatar from 'components/firebase comps/uploadAvatar';
 
 //component for Editpage display, defining label,value and onchange as passable values
 const FormField = props => {
@@ -23,18 +26,29 @@ FormField.propTypes = {
 
 const EditProfile = () => { //setting start states of username and birthday
   const history = useHistory();
+  const userId = useParams().userId;
+  const [user, setUser] = useState([]);
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+  const [avatarUrl, setAvatarUrl] = useState(null);
   const [username, setUsername] = useState(null);
-  const [birthday, setBirthday] = useState(null);
+  const [name, setName] = useState(null);
+  const [bio, setBio] = useState(null);
+
+  const types = ['image/png', 'image/jpeg'];
+  
  
   const edit = async () => {
     try {  
-      if(localStorage.getItem('userId')==localStorage.getItem('redirectedId')){
-        const id=localStorage.getItem('userId');
+      if(localStorage.getItem('userId') == user.userId){
         const requestBody = JSON.stringify({
-          "userId": id,
-          "username": username
+          "userId": userId,
+          "username": username,
+          "avatarUrl": avatarUrl,
+          "name": name,
+          "bio": bio
       });
-        await api.put("/users/"+ localStorage.getItem("redirectedId"), requestBody);
+        await api.put("/users/"+ userId, requestBody);
         history.push(`/Profile`);
       }
       else{
@@ -43,12 +57,6 @@ const EditProfile = () => { //setting start states of username and birthday
 
     } catch (error) {
       alert(`Something went wrong during the edit: \n${handleError(error)}`);}};
-
-      const [file, setFile] = useState(null);
-      const [error, setError] = useState(null);
-      const [imageUrl, setImageUrl] = useState(null);
-    
-      const types = ['image/png', 'image/jpeg'];
     
       const handleChange = (e) => {
         let selected = e.target.files[0]; // to select the first file (in order someone selects more files)
@@ -63,28 +71,58 @@ const EditProfile = () => { //setting start states of username and birthday
         }
       };
 
-      let uploadPhoto = (
-        <div className = "uploadImage"style={{ height:170 }}>
-          <div className = "uploadImage input"style={{ height:1 }}>
-          <label>
-            <input type="file" onChange={handleChange}   />
-          </label>
-          </div >
-          { error && <div className="uploadImage output"><div className="error">{ error }</div></div>}
-          { file && <div className="uploadImage output"style={{ width:100, height:100 }}><ProgressBar  file={file} setFile={setFile} imageUrl={imageUrl} setImageUrl={setImageUrl} /> </div>}
-        </div>
-      )
+      
+  // the effect hook can be used to react to change in your component.
+  // in this case, the effect hook is only run once, the first time the component is mounted
+  // this can be achieved by leaving the second argument an empty array.
+  // for more information on the effect hook, please see https://reactjs.org/docs/hooks-effect.html
+  useEffect(() => {
+    // effect callbacks are synchronous to prevent race conditions. So we put the async function inside:
+    const fetchData = async () => {
+      try {
+        const response =  await api.get('/users/'+ userId );
+
+        setUser(response.data);
+
+        console.log('request to:', response.request.responseURL);
+        console.log('status code:', response.status);
+        console.log('status text:', response.statusText);
+        console.log('requested data:', response.data);
+      } catch (error) {
+        console.error(`Something went wrong while getting the data for the user: \n${handleError(error)}`);
+        console.error("Details:", error);
+        alert("Something went wrong while getting the data for the user! See the console for details.");
+      }
+
+    }
+
+      fetchData()
+    }, []);
 
   return (   
-  <BaseContainer>
-    <div className="editProfile container">
-      <div className="editProfile form">
-        <FormField label="Username" value={username} onChange={un => setUsername(un)}/>
-        {uploadPhoto}
-        <div className="loginbutton-container"></div>
-        <Button  width="100%"onClick={() => edit()}>Save</Button>
+    <div><Navbar />
+      <div className="editProfile">
+        <div className="editProfile container">
+          <div className="editProfile form">
+          <div>Click on the avatar below to upload a new avatar image.</div>
+              <div class="image-upload">
+          
+                <label for="file-input">
+                  {user.avatarUrl && <img alt="Avatar" src={user.avatarUrl}></img>}
+                  {!user.avatarUrl && <img alt="Avatar" src={noAvatar}></img>}
+                </label>
+                <input id="file-input" type="file" onChange={handleChange}/>
+            </div>
+            { error && <div className="uploadAvatar output"><div className="error">{ error }</div></div>}
+            { file && <div className="uploadAvatar output"><UploadAvatar file={file} setFile={setFile} avatarUrl={avatarUrl} setAvatarUrl={setAvatarUrl} /> </div>}
+            <FormField label="Username" value={username} onChange={un => setUsername(un)}/>
+            <FormField label="Name" value={name} onChange={na => setName(na)}/>
+            <FormField label="Bio" value={bio} onChange={bi => setBio(bi)}/>
+            <div className="loginbutton-container"></div>
+            <Button  width="100%"onClick={() => edit()}>Save Profile</Button>
+          </div>
         </div>
       </div>
-  </BaseContainer>);};
+    </div>);};
 
 export default EditProfile;
